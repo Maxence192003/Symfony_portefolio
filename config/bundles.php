34a7ec6982a3096fbd7974a06afc3a43c0cd7319
Maxence192003 <maxence192003@gmail.com>
2026-03-30 09:03:58 +0200

    Fix: Remove DebugBundle and WebProfilerBundle - not available in minimal stack

--- config/bundles.php
+++ config/bundles.php
@@ -3,8 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
